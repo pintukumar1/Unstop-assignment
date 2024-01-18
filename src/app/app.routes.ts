facda02e6b6d94f1adec0adc007bf42188f462e6
Pintu Kumar <pintu@opskube.com>
2024-01-18 17:34:49 +0530

    added seat component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SeatInputComponent } from './seat-input/seat-input.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/seat-input-screen', pathMatch: 'full' },
+    { path: 'seat-input-screen', component: SeatInputComponent },
+    { path: 'seat-display-screen', component: SeatInputComponent },
+];
--- src/app/seat-input/seat-input.component.ts
+++ src/app/seat-input/seat-input.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   selector: 'app-seat-input',
   templateUrl: './seat-input.component.html',
   styleUrl: './seat-input.component.scss'
 })
-export class SeatInputComponent {
 
+export class SeatInputComponent implements OnInit{
+  seatInputForm!: FormGroup;
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {
+    this.seatInputForm = this.fb.group({
+      numberOfSeats: new FormControl('', [Validators.min(1), Validators.max(7)])
+    });
+  }
+
+  onSubmit() {
+
+    // Handling Form Submission;
+    if (this.seatInputForm.valid) {
+      // Navigate to seat display screen;
+      this.router.navigate(['/seat-display-screen'], { queryParams: { numberOfSeats: this.seatInputForm.value.numberOfSeats }} );
+    } 
+  }
 }
